3a35634bd3f3847ca2124d7d34239f7b3ea09281
Reine Alexandre Morato <47108497+moratomicro@users.noreply.github.com>
2022-06-10 16:22:53 -0300

    Add files via upload

---
+++ Jquery_essentials/01-jquery-essentials/script.js
@@ -0,0 +1,21 @@
+$(function(){
+    $(".jquery_essentials").html("jQuery Essentials!");
+    
+    console.group("Mensagens Gerais");
+    console.log("jQuery Essentials!");
+    console.info("O jQuery foi carregado com sucesso!");
+    console.warn("Um alerta!");
+    console.error("Um erro!");
+    console.groupEnd();
+    
+    var cadastro = true;
+    
+    console.group("Cadastro de usuários");
+    console.info("Cadastro iniciado!");
+    if(cadastro === true){
+        console.log("Cadastro Realizado");
+    }else {
+        console.warn("Erro ao cadastrar");
+    }
+    console.groupEnd();
+});
---
+++ Jquery_essentials/01-jquery-essentials/script_01.js
@@ -0,0 +1,24 @@
+//FORMA PADRÃO DE INICIALIZAR O jQUERY
+///$(document).ready(function(){
+//    $(".jquery_essentials").html("jQuery Iniciado!");
+//});
+
+//FORMA PADRÃO DE INICIALIZAR O jQUERY SIMPLIFICADA
+//$(function(){
+//    $(".jquery_essentials").html("jQuery Iniciado com Gatilho!");
+//});
+
+//CARREGANDO ANTES DE OUTRA LIB
+//jQuery(function($){
+//    $(".jquery_essentials").html("jQuery Antes de Outra Lib!");
+//});
+
+//CARREGANDO DEPOIS DE OUTRA LIB
+//var $j = jQuery.noConflict();
+//$j(document).ready(function(){
+//    $j(".jquery_essentials").html("jQuery Depois de Outra Lib!");
+//});
+
+$(function(){
+    $(".jquery_essentials").html("jQuery Essentials!");
+});
